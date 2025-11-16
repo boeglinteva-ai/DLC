82082feb4b0bf74305d0b7072bde09f85fe24003
boeglinteva-ai <boeglin.teva@gmail.com>
2025-11-16 20:34:44 +0100

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ⚠️ CE FICHIER DOIT RESTER PRIVÉ ⚠️
 const ONEDRIVE_CONFIG = {
-    clientId: "4c6a4fb0-1ce2-4ad8-a5a1-cb7a0b42b5db", 
+    clientId: "4c6a4fb0-1ce2-4ad8-a5a1-cb7a0b42b5db",
     redirectUri: "https://boeglinteva-ai.github.io/DLC/onedrive-callback.html",
-    scopes: "Files.ReadWrite.All offline_access"
+    scopes: "openid profile email offline_access Files.ReadWrite.All"
 };
