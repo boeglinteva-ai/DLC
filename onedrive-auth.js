eb550c41b4b796aedc5fcfe1f97e26bb71921754
boeglinteva-ai <boeglin.teva@gmail.com>
2025-11-16 20:11:37 +0100

    Update onedrive-auth.js

--- onedrive-auth.js
+++ onedrive-auth.js
@@ -2,7 +2,7 @@ async function loginOneDrive() {
     const authUrl =
         "https://login.microsoftonline.com/common/oauth2/v2.0/authorize" +
         `?client_id=${ONEDRIVE_CONFIG.clientId}` +
-        "&response_type=token" +
+        "&response_type=code" +
         `&redirect_uri=${encodeURIComponent(ONEDRIVE_CONFIG.redirectUri)}` +
         `&scope=${encodeURIComponent(ONEDRIVE_CONFIG.scopes)}`;
 
