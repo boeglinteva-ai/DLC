87320fdd8379701ed30ec75c30b0b0a90557b4c4
boeglinteva-ai <boeglin.teva@gmail.com>
2025-11-16 15:28:51 +0100

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('padova-v1').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'Logo-DLC.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
